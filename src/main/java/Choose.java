e53098376ddc1377bc1473054ec320f267c52eb4
captainoffool <durnev.aa1994@yandex.ru>
2018-09-14 19:27:34 +0300

    ADd choose class

---
+++ src/main/java/Choose.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Choose {
+    public void dgdf() {
+        System.out.print("Выберите операцию:");
+        System.out.println("1.Калькулятор");
+        System.out.println("2. Выбор максимальной длины слова из массива");
+        Scanner check = new Scanner(System.in);
+        String x = check.next();
+        if (x.equals("1")) {
+            System.out.println("Выбран калькулятор");
+        } else if (x != "1") {
+            System.out.println("kdsjgfbdukgjiubdfgidfb");
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Choose choose = new Choose();
+        choose.dgdf();
         Scanner in = new Scanner(System.in);
         System.out.print("Введите первое число: ");
         double numb1 = in.nextDouble();
@@ -17,7 +19,7 @@ public class Main {
         String op = in.next();
         if (op.equals("+")) {
             System.out.print("Ответ = ");
-            System.out.printf("%.4f", (numb1 + numb2));
+            System.out.printf("%.4f", (numb1 + numb2 ));
         }
         if (op.equals("-")) {
             System.out.print("Ответ = ");
